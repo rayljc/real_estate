c204b3969dd3f0d7f886045b595c6360a60dc834
rayjclin <nturay0321@gmail.com>
2019-08-19 16:18:46 -0400

    Add postgres db and its settings

--- realestate/settings.py
+++ realestate/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestatedb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
