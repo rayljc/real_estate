93e1696e832919cba9b69199af49418e335739d8
rayjclin <nturay0321@gmail.com>
2019-08-19 22:42:29 -0400

    Add message in settings.py

--- contacts/views.py
+++ contacts/views.py
@@ -30,7 +30,7 @@ def contact(request):
 
         contact.save()
 
-        Send email
+        # Send email
         send_mail(
           'Property Listing Inquiry',
           'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
